87c96b88ab661b889a5f539a84851dfacd02d3a4
ZhuRuoLing <jkl-963@live.com>
2024-04-10 11:33:03 +0800

    1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "icu.takeneko.neko.medibox"
         minSdk = 29
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/icu/takeneko/neko/medibox/data/UsageType.kt
+++ app/src/main/java/icu/takeneko/neko/medibox/data/UsageType.kt
@@ -4,8 +4,8 @@ import icu.takeneko.neko.medibox.R
 
 enum class UsageType(val resId: Int, val descriptorStringRes: Int) : Identifiable<UsageType> {
     MILLIGRAM(R.string.text_mg, R.string.text_desc_mg),
-    PILL(R.string.text_one_pill, R.string.text_one_pill),
-    HALF_PILL(R.string.text_half_pill, R.string.text_half_pill);
+    PILL(R.string.text_one_pill, R.string.text_desc_one_pill),
+    HALF_PILL(R.string.text_half_pill, R.string.text_desc_half_pill);
 
     override fun id() = this.ordinal
     override fun match(id: Int): UsageType {
